029aaf0566c53fb9307b89052cf358111a26c508
Kentaro Wada <www.kentaro.wada@gmail.com>
2017-07-21 05:31:40 +0900

    Fix evaluate.py to work completely

--- evaluate.py
+++ evaluate.py
@@ -3,64 +3,18 @@ from probabilistic_segmentation import ProbabilisticSegmentationRF, Probabilisti
 import pickle
 import os
 
+import fcn
 import matplotlib.pyplot as plt
 import numpy as np
 import copy
-import rospkg
-
-
-def _fast_hist(a, b, n):
-    k = (a >= 0) & (a < n)
-    hist = np.bincount(n * a[k].astype(int) +
-                       b[k], minlength=n**2).reshape(n, n)
-    return hist
-
-def label_accuracy_score(label_true, label_pred, n_class):
-    """Returns accuracy score evaluation result.
-      - overall accuracy
-      - mean accuracy
-      - mean IU
-      - fwavacc
-    """
-    hist = _fast_hist(label_true.flatten(), label_pred.flatten(), n_class)
-    acc = np.diag(hist).sum() / hist.sum().astype(np.float64)
-    acc_cls = np.diag(hist) / hist.sum(axis=1).astype(np.float64)
-    acc_cls = np.nanmean(acc_cls)
-    iu = np.diag(hist) / (hist.sum(axis=1) + hist.sum(axis=0) - np.diag(hist)).astype(np.float64)
-    mean_iu = np.nanmean(iu)
-    freq = hist.sum(axis=1) / hist.sum().astype(np.float64)
-    fwavacc = (freq[freq > 0] * iu[freq > 0]).sum()
-    return acc, acc_cls, mean_iu, fwavacc
-
-
-# previously declared in main.py
-def combine_datasets(datasets):
-    samples = []
-    for d in datasets:
-        samples += d.samples
-    return APCDataSet(samples=samples)
-
-
-def load_datasets(dataset_names, data_path, cache_path):
-    datasets = dict()
-
-    for dataset_name in dataset_names:
-        dataset_path = os.path.join(
-                data_path, 'rbo_apc/{}'.format(dataset_name))
-        datasets[dataset_name] = APCDataSet(
-                name=dataset_name, dataset_path=dataset_path,
-                cache_path=cache_path, load_from_cache=True)
-    return datasets
-
-
 
 
 def evaluate(bp, test_data):
-    acc_list = []
-    acc_cls_list = []
-    mean_iu_list = []
-    fwavacc_list = []
+    lbl_preds = []
+    lbl_trues = []
     for sample in test_data.samples:
+        sample.candidate_objects = test_data.object_names
+
         if len(sample.object_masks) == 0:
             continue
         pred_target = sample.object_masks.keys()[0]
@@ -69,59 +23,48 @@ def evaluate(bp, test_data):
                 continue
             pred_target = sample.object_masks.keys()[1]
         bp.predict(sample, pred_target)
-        print 'done'
 
         images = []
-        images.append(bp.posterior_images_smooth['shelf'])
-        objects = []
-        objects.append('shelf')
-        for _object in  bp.posterior_images_smooth.keys():
-            if _object != 'shelf':
+        for _object in test_data.object_names:
+            if _object in bp.posterior_images_smooth:
                 images.append(bp.posterior_images_smooth[_object])
-                objects.append(_object)
-        pred = np.argmax(np.array(images), axis=0)
+            else:
+                raise ValueError
+                images.append(np.zeros_like(images[0]))
+        pred = np.argmax(np.array(images), axis=0).astype(np.int32)
 
-
-
-        # remove dataset that does not have complete set
-        objects_copy = copy.copy(objects)
-        object_masks_keys = sample.object_masks.keys()
-        if 'shelf' in objects_copy: objects_copy.remove('shelf')
-        if 'shelf' in object_masks_keys: object_masks_keys.remove('shelf')
-        if set(objects_copy) != set(object_masks_keys):
-            #print 'skip posterior_image keys ', objects_copy
-            #print 'skip object_mask keys ', object_masks_keys
-            continue
         true = np.zeros_like(pred)
+        for obj_id, obj in enumerate(test_data.object_names):
+            if obj != 'shelf' and obj in sample.object_masks:
+                true[sample.object_masks[obj]] = obj_id
 
+        pred[~sample.bin_mask] = -1
+        true[~sample.bin_mask] = -1
 
-        for i, _object in enumerate(objects):
-            if _object != 'shelf':
-                true[sample.object_masks[_object]] = i
+        lbl_preds.append(pred)
+        lbl_trues.append(true)
 
-        masked_pred = pred[sample.bin_mask] 
-        masked_true = true[sample.bin_mask]
-
-
-        acc, acc_cls, mean_iu, fwavacc = label_accuracy_score(masked_true, masked_pred, len(objects))
-        acc_list.append(acc)
-        acc_cls_list.append(acc_cls)
-        mean_iu_list.append(mean_iu)
-        fwavacc_list.append(fwavacc)
-        
-        """
-        label_pred = np.zeros(pred.shape[1:]).astype(np.int64)
-        label_true = np.zeros(pred.shape[1:]).astype(np.int64)
-
-        for i in range(pred.shape[0]):
-            label_pred[pred[i]] = i
-            label_true[true[i]] = i
-        label_pred_masked = label_pred[sample.bin_mask]
-        label_true_masked = label_true[sample.bin_mask]
-        """
-    return acc_list, acc_cls_list, mean_iu_list, fwavacc_list
-
-
+        # import mvtk
+        # import cv2
+        # image = cv2.imread(sample.file_prefix + '.jpg')[:, :, ::-1]
+        # x1 = sample.bounding_box['x']
+        # y1 = sample.bounding_box['y']
+        # x2 = x1 + sample.bounding_box['w']
+        # y2 = y1 + sample.bounding_box['h']
+        # image = image[y1:y2, x1:x2]
+        # image[~sample.bin_mask] = 0
+        # #
+        # pred_viz = mvtk.image.label2rgb(pred)
+        # true_viz = mvtk.image.label2rgb(true)
+        # #
+        # pred_viz = mvtk.image.tile([image, pred_viz], shape=(1, 2))
+        # true_viz = mvtk.image.tile([image, true_viz], shape=(1, 2))
+        # viz = mvtk.image.tile([true_viz, pred_viz], shape=(2, 1))
+        # #
+        # cv2.imshow('viz', viz[:, :, ::-1])
+        # cv2.waitKey(0)
+    n_class = len(test_data.object_names)
+    return fcn.utils.label_accuracy_score(lbl_trues, lbl_preds, n_class)
 
 
 def create_dataset(dataset_path):
@@ -136,76 +79,35 @@ def create_dataset(dataset_path):
                 data_file_prefixes.append(
                     os.path.join(dir_name, f[:-len(key)]))
 
-    print data_file_prefixes
     for file_prefix in data_file_prefixes:
         dataset.samples.append(
             APCSample(data_2016_prefix=file_prefix,
                         labeled=True, is_2016=True, infer_shelf_mask=True))
     return dataset
-        
 
 
+def main():
+    dataset_path = '/home/wkentaro/data/datasets/APC2016/APC2016rbo'
+    dataset = create_dataset(dataset_path)
+    dataset_train, dataset_valid = dataset.split_simple(portion_training=0.7)
 
-###############################################################################
-#                               prepare dataset                               #
-###############################################################################
-#data_path = '/home/leus/ros/indigo/src/start-jsk/jsk_apc/jsk_apc2016_common/data'
-#cache_path = os.path.join(data_path, 'cache')
-#dataset_path = os.path.join(data_path, 'rbo_apc')
-rospack = rospkg.RosPack()
-
-common_path = rospack.get_path('jsk_apc2016_common')
-data_path = common_path + '/data/'
-dataset_name = 'tokyo_run/single_item_labeled'
-dataset_path = os.path.join(data_path, dataset_name)
-
-
-data = create_dataset(dataset_path)
-
-
-###############################################################################
-#                                   dataset                                   #
-###############################################################################
-train_data, test_data = data.split_simple(portion_training=0.7)
-
-
-###############################################################################
-#                                all features                                 #
-###############################################################################
-all_features = ['color', 'height3D', 'dist2shelf']
-params = {
-        'use_features': all_features,
-        'segmentation_method': "max_smooth", 'selection_method': "max_smooth",
-        'make_convex': True, 'do_shrinking_resegmentation': True,
-        'do_greedy_resegmentation': True}
-bp = ProbabilisticSegmentationBP(**params)
-
-
-bp.fit(train_data)
-
-acc_list, acc_cls_list, mean_iu_list, fwavacc_list = evaluate(bp, test_data)
-        
-print 'all features acc ', np.mean(acc_list)
-print 'all features acc_cls ', np.mean(acc_cls_list)
-print 'all features mean_iu ', np.mean(mean_iu_list)
-print 'all features fwavcc ', np.mean(fwavacc_list)
-
-
-###############################################################################
-#                                # Color only                                 #
-###############################################################################
-params = {
+    params = {
         'use_features': ['color'],
-        'segmentation_method': "max_smooth", 'selection_method': "max_smooth",
-        'make_convex': True, 'do_shrinking_resegmentation': True,
-        'do_greedy_resegmentation': True}
-bp = ProbabilisticSegmentationBP(**params)
+        'segmentation_method': "max_smooth",
+        'selection_method': "max_smooth",
+        'make_convex': True,
+        'do_shrinking_resegmentation': True,
+        'do_greedy_resegmentation': True,
+    }
+    clf = ProbabilisticSegmentationBP(**params)
+    clf.fit(dataset_train)
+    acc, acc_cls, mean_iu, fwavacc = evaluate(clf, dataset_valid)
 
-bp.fit(train_data)
-acc_list, acc_cls_list, mean_iu_list, fwavacc_list = evaluate(bp, test_data)
-        
-print 'trained only by color features acc ', np.mean(acc_list)
-print 'trained only by color features acc_cls ', np.mean(acc_cls_list)
-print 'trained only by color features mean_iu ', np.mean(mean_iu_list)
-print 'trained only by color features fwavcc ', np.mean(fwavacc_list)
+    print 'trained only by color features acc ', acc
+    print 'trained only by color features acc_cls ', acc_cls
+    print 'trained only by color features mean_iu ', mean_iu
+    print 'trained only by color features fwavcc ', fwavacc
 
+
+if __name__ == '__main__':
+    main()
